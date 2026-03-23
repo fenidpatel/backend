53bc543a6aef37e3015f589edf60152f332df344
fenidpatel <fenidpatel154@gmail.com>
2026-03-23 13:54:06 -0500

    Add 'password' to fillable properties in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email'])]
+#[Fillable(['name', 'email', 'password'])]
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
