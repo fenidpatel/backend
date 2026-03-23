05d6c7ab781ccc8c008b0482f299409d6234bed9
fenidpatel <fenidpatel154@gmail.com>
2026-03-23 14:15:46 -0500

    Add files via upload

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+Route::post('/users',        [UserController::class, 'store']);
+Route::get('/users/{id}',    [UserController::class, 'show']);
+Route::put('/users/{id}',    [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
